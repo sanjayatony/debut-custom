27395e3feb941e18776d21942b82fc020ea1980b
Tony S <sanjayatony@gmail.com>
2021-02-28 20:41:53 +0700

    themekit + tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  purge: {
+    enabled: false,
+    content: [
+      "./layout/*.liquid",
+      "./templates/*.liquid",
+      "./sections/*.liquid",
+      "./snippets/*.liquid",
+    ],
+  },
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
